67ac9b2253b5fb27c58a6d3df3011541dbae7639
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-22 14:57:41 +0000

    [Add] ビューで画像が表示されるようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_21_141238) do
+ActiveRecord::Schema.define(version: 2024_02_22_141749) do
 
   create_table "posts", force: :cascade do |t|
     t.string "location"
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_id"
   end
 
 end
